207828f1a37688dbe23332903de542082ecb0f51
Bruno Patrício <bpatriciocosta@gmail.com>
2023-10-08 21:35:10 -0300

    App and Server Config

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+	const APP_URL="http://localhost:8080/main-magazord-backend-test-main/";
+	const APP_NAME="Lista de Contatos da Magazord";
+	const APP_SESSION_NAME="CRUD";
+
+	date_default_timezone_set("America/Sao_Paulo");
